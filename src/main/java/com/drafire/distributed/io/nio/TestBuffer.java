bbece3eb29ae23668c47daf9cb249a47b6215fa8
pangtinghuan <pangtinghuan@tuandai.com>
2018-11-14 18:39:15 +0800

    增加NIO的Bytebuffer练习

---
+++ src/main/java/com/drafire/distributed/io/nio/TestBuffer.java
@@ -0,0 +1,26 @@
+package com.drafire.distributed.io.nio;
+
+import java.nio.ByteBuffer;
+
+public class TestBuffer {
+    //Bytebuffer a里的切片buffer b，会拥有自己的position、limit，mark会是null。有且只有a是只读的，b才是只读的；
+    // 有且只有a是direct的，b才是direct
+    //b改变，会影响a的内容（因为本来就是同一个buffer）
+
+    public static void main(String[] args) {
+        //put byte 是可以的，但是put int就不行了，为什么？
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        for (int i = 0; i < buffer.capacity(); i++) {
+            System.out.println(i + "--" + buffer.capacity() + "--" + buffer.limit());
+            String a = i + "";
+            buffer.put(Byte.parseByte(a));
+        }
+        buffer.flip();
+        buffer.position(0);
+        buffer.limit(buffer.capacity());
+
+        while (buffer.remaining() > 0) {
+            System.out.println("[" + buffer.get() + "]");
+        }
+    }
+}
