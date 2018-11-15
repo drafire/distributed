ba24b98479325a9ee30546c2a1c5ad3f9f81c49e
pangtinghuan <pangtinghuan@tuandai.com>
2018-11-15 15:49:13 +0800

    增加NIO的api说明

--- src/main/java/com/drafire/distributed/netty/DiscardServer.java
+++ src/main/java/com/drafire/distributed/netty/DiscardServer.java
@@ -17,7 +17,13 @@ public class DiscardServer {
     }
 
     public void run() throws Exception {
+        //Group：群组，Loop：循环，Event：事件，这几个东西联在一起，相比大家也大概明白它的用途了。
+        //Netty内部都是通过线程在处理各种数据，EventLoopGroup就是用来管理调度他们的，注册Channel，管理他们的生命周期。
+        //NioEventLoopGroup是一个处理I/O操作的多线程事件循环
+        //bossGroup作为boss,接收传入连接
+        //因为bossGroup仅接收客户端连接，不做复杂的逻辑处理，为了尽可能减少资源的占用，取值越小越好
         EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        //workerGroup作为worker，处理boss接收的连接的流量和将接收的连接注册进入这个worker
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap(); // (2)
